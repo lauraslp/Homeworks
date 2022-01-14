ef50f51fd4eabcec0907d4ff3cd713137f7db7ae
lauraslp <laura.slepikaite@gmail.com>
2022-01-14 22:08:30 +0200

    Homework (decorators)

--- homework.py
+++ homework.py
@@ -1,22 +1,3 @@
-# Write a program that uses the filter function to print words which are palindromes in the word list passed.
-# A palindrome is an expression that sounds the same when read from both the left and the right side.
-# For example, such words are palindromes: radar, level, rotor.
-# word_list = ['level', 'madam', 'sister', 'kayak', 'book', 'rotator', 'runner', 'ice' ]
-
-# Function
-# def is_polindrome(word_list):
-#     polindrome_list = []
-#     for word in word_list:
-#         if word == word[::-1]:
-#             polindrome_list.append(word)
-#     return polindrome_list
-#
-# print(is_polindrome(word_list))
-
-# Lambda
-# polindrome_list = list(filter(lambda word: word == word[::-1], word_list))
-# print(polindrome_list)
-
 # Decorators
 # # 1. Write a decorator that will allow you to execute code of a decorated function only during the day
 # # (between 7 and 22).
