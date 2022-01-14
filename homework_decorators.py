25d89420dd74ddfe5c27ef3dc6702ba44ccfa638
lauraslp <laura.slepikaite@gmail.com>
2022-01-14 23:12:59 +0200

    Trying to upload some changes

--- homework_decorators.py
+++ homework_decorators.py
@@ -1,16 +1,17 @@
 # Decorators
-# # 1. Write a decorator that will allow you to execute code of a decorated function only during the day
-# # (between 7 and 22).
+# 1. Write a decorator that will allow you to execute code of a decorated function only during the day
+# (between 7 and 22).
 # from _datetime import datetime as d
 #
 # #function that decorates other function. Decorator
 # def decorate_my_function(func):
 #     def execute_function():
-#         if int(d.now().strftime('%d')) > 7 and int(d.now().strftime('%d')) < 22:
+#         if int(d.now().strftime('%H')) >= 7 and int(d.now().strftime('%H')) < 22:
 #             func()
-#             print(f'Today is {d.now().strftime("%Y-%m-%d")} and {func.__name__} was executed')
+#             print(f'Time now is {d.now().strftime("%H:%M")} and {func.__name__} was executed')
 #         else:
-#             print(f'Today is {d.now().strftime("%Y-%m-%d")} and {func.__name__} can not be executed!')
+#             pass
+#             print(f'Time now is {d.now().strftime("%H:%M")} and {func.__name__} can not be executed!')
 #     return execute_function
 #
 # # function that will be decorated
@@ -26,16 +27,18 @@
 # # unavailable.
 # from datetime import datetime as d
 #
-# def decorate_function(func):
-#     def execute_function():
-#         if int(d.now().strftime("%H")) <= 7 and int(d.now().strftime("%H")) >= 22:
-#             print(f'Time now is {d.now().strftime("%H:%M")} and {func.__name__} can not be executed!')
-#         else:
-#             func()
-#             print(f'Time now is {d.now().strftime("%H:%M")} and {func.__name__} was executed')
-#     return execute_function
+# def run_only_between(_start = 7, _end = 22):
+#     def decorate_function(func):
+#         def execute_function():
+#             if int(d.now().strftime("%H")) < _start or int(d.now().strftime("%H")) >= _end:
+#                 print(f'Time now is {d.now().strftime("%H:%M")} and {func.__name__} can not be executed!')
+#             else:
+#                 func()
+#                 print(f'Time now is {d.now().strftime("%H:%M")} and {func.__name__} was executed')
+#         return execute_function
+#     return decorate_function
 #
-# @decorate_function
+# @run_only_between(7,22)
 # def my_function():
 #     print('This is my function')
 # my_function()
@@ -43,23 +46,21 @@
 # 2. Write a decorator that will show the hour, minute and second before calling the code of the decorated
 # function and after calling the code of the decorated function.
 
-from _datetime import datetime as d
-import time as t
-
-def decorate_my_function(func):
-    def print_time():
-        print(f'Time before {func.__name__} {d.now().strftime("%H:%M:%S")}')
-        func()
-        print(f'Time after {func.__name__} {d.now().strftime("%H:%M:%S")}')
-    return print_time
-
-# @ leidzia nerasyti my_function = decorate_my_function(my_function)()
-@decorate_my_function
-def my_function():
-    print(f'My function was executed')
-    t.sleep(2)
-
-# my_function = decorate_my_function(my_function)()
-my_function()
-
-# trying to upload changes
+# from _datetime import datetime as d
+# import time as t
+#
+# def decorate_my_function(func):
+#     def print_time():
+#         print(f'Time before {func.__name__} {d.now().strftime("%H:%M:%S")}')
+#         func()
+#         print(f'Time after {func.__name__} {d.now().strftime("%H:%M:%S")}')
+#     return print_time
+#
+# # @ leidzia nerasyti my_function = decorate_my_function(my_function)()
+# @decorate_my_function
+# def my_function():
+#     print(f'My function was executed')
+#     t.sleep(2)
+#
+# # my_function = decorate_my_function(my_function)()
+# my_function()
